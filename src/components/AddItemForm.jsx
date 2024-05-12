6af900c44ca5c629758ff82a5190761b3eb834a6
adrians90 <adriansandu9090@gmail.com>
2024-05-12 17:33:30 +0100

    Add basic validation for the input field on AddItemForm

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 
 function AddItemForm({ setItems }) {
   const [itemText, setItemText] = useState("");
+  const inputRef = useRef();
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+
+        //basic validation
+        if (!itemText) {
+          alert("Item can't be empty");
+          inputRef.current.focus();
+          return;
+        }
+        const newItem = {
+          id: new Date().getTime(),
+          name: itemText,
+          packed: false,
+        };
+        setItems((prev) => [...prev, newItem]);
       }}
     >
       <h2>Add an item</h2>
       <input
+        ref={inputRef}
         value={itemText}
         type="text"
         onChange={(e) => {
           setItemText(e.target.value);
         }}
+        autoFocus={true}
       />
       <Button>Add to list</Button>
     </form>
