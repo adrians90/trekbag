bb1b07330988775ab34911dc2e32ee74a156f113
adrians90 <adriansandu9090@gmail.com>
2024-05-12 15:24:24 +0100

    add copyright to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <small>&copy; 2024. Copyright by Adrian Sandu</small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
