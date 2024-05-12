1a1e146635535199e505e136632c17a4cb661645
adrians90 <adriansandu9090@gmail.com>
2024-05-12 17:00:37 +0100

    Move initialItems and secondaryButtons in constants.js file

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,11 +1,5 @@
 import Button from "./Button";
-
-const secondaryButtons = [
-  "Mark all as complete",
-  "Mark all as incomplete",
-  "Reset to initial",
-  "Remove all items",
-];
+import { secondaryButtons } from "./lib/constants";
 
 function ButtonGroup() {
   return (
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,20 +1,4 @@
-const initialItems = [
-  {
-    id: 1,
-    name: "good mood",
-    packed: true,
-  },
-  {
-    id: 2,
-    name: "passport",
-    packed: false,
-  },
-  {
-    id: 3,
-    name: "phone charger",
-    packed: false,
-  },
-];
+import { initialItems } from "./lib/constants";
 
 function ItemList() {
   return (
