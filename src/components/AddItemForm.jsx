8532248ea7546ef793e108ef6846d81374220a9c
adrians90 <adriansandu9090@gmail.com>
2024-05-12 17:37:40 +0100

    Move form submit logic to handleSubmit function

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -4,26 +4,26 @@ import Button from "./Button";
 function AddItemForm({ setItems }) {
   const [itemText, setItemText] = useState("");
   const inputRef = useRef();
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
 
-        //basic validation
-        if (!itemText) {
-          alert("Item can't be empty");
-          inputRef.current.focus();
-          return;
-        }
-        const newItem = {
-          id: new Date().getTime(),
-          name: itemText,
-          packed: false,
-        };
-        setItems((prev) => [...prev, newItem]);
-        setItemText("");
-      }}
-    >
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //basic validation
+    if (!itemText) {
+      alert("Item can't be empty");
+      inputRef.current.focus();
+      return;
+    }
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+    setItems((prev) => [...prev, newItem]);
+    setItemText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
       <input
         ref={inputRef}
