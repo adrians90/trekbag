e5cf84679547c43a89c33a7a84f070878d0de31a
adrians90 <adriansandu9090@gmail.com>
2024-05-12 15:55:57 +0100

    Create AddItemForm and ButtonGroup components and import in Sidebar

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,5 @@
+function AddItemForm() {
+  return <div></div>;
+}
+
+export default AddItemForm;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,13 @@
+import AddItemForm from "./AddItemForm";
+import ButtonGroup from "./ButtonGroup";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <AddItemForm />
+      <ButtonGroup />
+    </div>
+  );
 }
 
 export default Sidebar;
