ce14dc28653a7d6f9676b63d62854e9666a35d93
adrians90 <adriansandu9090@gmail.com>
2024-05-12 16:58:15 +0100

    Add initial items object array

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,13 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
 function ButtonGroup() {
-  const secondaryButtons = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initial",
-    "Remove all items",
-  ];
   return (
     <section className="button-group">
       {secondaryButtons.map((text) => {
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,39 @@
+const initialItems = [
+  {
+    id: 1,
+    name: "good mood",
+    packed: true,
+  },
+  {
+    id: 2,
+    name: "passport",
+    packed: false,
+  },
+  {
+    id: 3,
+    name: "phone charger",
+    packed: false,
+  },
+];
+
 function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {initialItems.map((item) => {
+        return <Item key={item.name} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" /> {item.name}
+      </label>
+    </li>
+  );
 }
 
 export default ItemList;
