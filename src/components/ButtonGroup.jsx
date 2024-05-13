29a829cb73e4699338c87933347bda2d870e3159
adrians90 <adriansandu9090@gmail.com>
2024-05-13 16:35:56 +0100

    Fix type in ButtonGroup and removed ItemsContextProvider

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -3,8 +3,8 @@ import Button from "./Button";
 
 function ButtonGroup() {
   const markAllAsComplete = useItemsStore((state) => state.markAllAsComplete);
-  const markAllAsInComplete = useItemsStore(
-    (state) => state.markAllAsInComplete
+  const markAllAsIncomplete = useItemsStore(
+    (state) => state.markAllAsIncomplete
   );
   const resetToInitial = useItemsStore((state) => state.resetToInitial);
   const removeAllItems = useItemsStore((state) => state.removeAllItems);
@@ -14,7 +14,7 @@ function ButtonGroup() {
       <Button onClick={markAllAsComplete} buttonType="secondary">
         Mark all as complete
       </Button>
-      <Button onClick={markAllAsInComplete} buttonType="secondary">
+      <Button onClick={markAllAsIncomplete} buttonType="secondary">
         Mark all as incomplete
       </Button>
       <Button onClick={resetToInitial} buttonType="secondary">
