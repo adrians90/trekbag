f182c2aea700c635300469fe1694e5049deae357
adrians90 <adriansandu9090@gmail.com>
2024-05-13 15:32:06 +0100

    Refactor AddItemForm to use useContext

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import AddItemForm from "./AddItemForm";
 import ButtonGroup from "./ButtonGroup";
+import { ItemsContext } from "./contexts/ItemsContextProvider";
 
 function Sidebar() {
+  const { handleAddItem } = useContext(ItemsContext);
   return (
     <div className="sidebar">
-      <AddItemForm />
+      <AddItemForm onAddItem={handleAddItem} />
       <ButtonGroup />
     </div>
   );
