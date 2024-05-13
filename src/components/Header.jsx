a7d3a5d735760d192fa47a659a94ee33f4d61592
adrians90 <adriansandu9090@gmail.com>
2024-05-13 16:28:03 +0100

    Refactor Header component to import items from itemsStore

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
+import { useItemsStore } from "../stores/itemsStore";
 import Counter from "./Counter";
 import Logo from "./Logo";
 
-import { useItemsContext } from "./lib/hooks";
-
 function Header() {
-  const { items } = useItemsContext();
+  const items = useItemsStore((state) => state.items);
   return (
     <header>
       <Logo />
