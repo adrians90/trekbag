c82f5a082459b413820c272cc3d198c0a62ed70f
adrians90 <adriansandu9090@gmail.com>
2024-05-13 13:48:01 +0100

    Create sort algorithm for the sorting function

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import EmptyView from "./EmptyView";
 import Select from "react-select";
 
@@ -17,16 +18,31 @@ const sortingOptions = [
 ];
 
 function ItemList({ items, handleDeleteItem, handleToggleItem }) {
+  const [sortBy, setSortBy] = useState("default");
+  const sortedItems = items.sort((a, b) => {
+    if (sortBy == "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy == "unpacked") {
+      return a.packed - b.packed;
+    }
+
+    return;
+  });
   return (
     <ul className="item-list">
       {items.length === 0 && <EmptyView />}
 
       {items.length > 0 ? (
         <section className="sorting">
-          <Select options={sortingOptions} />
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
         </section>
       ) : null}
-      {items.map((item) => {
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.name}
